ef51e9afd890ead9925dabdbf69a85b2974f2027
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2024-07-17 21:26:31 +0700

    seperate file from index to database

--- backend/.src/database.js
+++ backend/.src/database.js
@@ -1,5 +1,20 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const MONGO_USERNAME = process.env.MONGO_INITDB_ROOT_USERNAME
-const MONGO_PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD
-const MONGO_URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@127.0.0.1:27017/Wealthtrack?authSource=admin`
+const MONGO_USERNAME = process.env.MONGO_INITDB_ROOT_USERNAME;
+const MONGO_PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
+
+const MONGO_URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:27017/${DB_NAME}?authSource=admin`;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        throw error;
+    }
+};
+
+module.exports = connectDB;
