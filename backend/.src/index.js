9681f9a2ff3fa370d95d580ac250633842193a4d
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2024-07-19 20:43:44 +0700

    use logger and serve frontend

--- backend/.src/index.js
+++ backend/.src/index.js
@@ -2,32 +2,40 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./database');
+const logger = require('./logger');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+// cannot access the process.env throw the error
+
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!!!!!');
-});
-
+// Serve static files from the frontend build directory
+app.use(express.static(path.join(__dirname, '../frontend_build')));
 app.get("/api/v1", (req, res) => {
     res.send('you are on the api/v1 route')
 })
 
 const startServer = async () => {
     try {
+        logger.info('Trying to connect to database...');
         await connectDB();
         app.listen(PORT, '0.0.0.0', () => {
-            console.log(`App listening on port ${PORT}`);
+            logger.debug(`App listening on port ${PORT}`);
+            if (logger.isDebugEnabled()) {
+                logger.info('Starting server in debug mode...');
+            } else {
+                logger.info('Starting server...');
+            }
+            logger.info('Press Ctrl+C to quit.');
         });
     } catch (error) {
-        console.error('Failed to start server:', error);
+        logger.error('Failed to start server:', error);
         process.exit(1);
     }
 };
 
-console.log('Starting server...');
+logger.info('Trying to start server...');
 startServer();
