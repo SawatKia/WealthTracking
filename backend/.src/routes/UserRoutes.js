a731371c6f72f3492904feffaff0b0a77a63e9af
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2024-07-22 21:41:22 +0700

    convert /create to /register and add error-handling middleware

--- backend/.src/routes/UserRoutes.js
+++ backend/.src/routes/UserRoutes.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const UserController = require('../Controllers/UserControlller');
-const logger = require('../configs/logger');
+const Logging = require('../configs/logger');
+const { AppError } = require('../utils/error')
 
 const router = express.Router();
 const UserCont = new UserController();
+const logger = new Logging('UserRoutes');
 
 router.get('/', (req, res) => {
     logger.info('request to / endpoint');
     res.send('Hello World, from UserRoutes');
 });
 // Route to create a new user
-router.post('/create', async (req, res) => {
-    try {
-        logger.info("request to /create endpoint");
-        // Call the createUser function from the UserController
-        const user = await UserCont.createUser(req.body);
+router.post('/register', UserCont.register.bind(UserCont));
 
-        // Send a response to the client
-        res.status(201).json({
-            message: 'User created successfully',
-            data: {
-                id: user._id,
-            }
+
+// Error-handling middleware
+router.use((err, req, res, next) => {
+    if (err instanceof AppError) {
+        res.status(err.statusCode).json({
+            status_code: err.statusCode,
+            message: err.message,
+            data: err.data || null
         });
-    } catch (error) {
-        // Handle any errors that occur during user creation
+    } else {
+        console.error(err); // Log the error for debugging purposes
         res.status(500).json({
-            message: 'Failed to create user',
-            error: error.message
+            status_code: 500,
+            message: 'Internal Server Error'
         });
     }
 });
