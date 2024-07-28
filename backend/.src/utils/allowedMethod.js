fef8dfc5c8cb1cdddd2aa2d34705be13b59f2a56
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2024-07-28 10:12:15 +0700

    update path follow best practices and modify MethodValidator to allow parameter

--- backend/.src/utils/allowedMethod.js
+++ backend/.src/utils/allowedMethod.js
@@ -3,12 +3,21 @@ const { MethodNotAllowedError, BadRequestError } = require('../utils/error');
 const MethodValidator = (allowedMethods) => {
     return (req, res, next) => {
         const { method, path } = req;
-        if (!allowedMethods[path]) {
+
+        // replace any id in the path with :userId
+        const pathWithParams = path.replace(/\/[0-9a-fA-F]{24}$/, '/:userId');
+        // Check if the exact path or the modified path exists in allowedMethods
+        if (!allowedMethods[path] && !allowedMethods[pathWithParams]) {
             return next(new BadRequestError(`${path} not available`));
         }
-        if (!allowedMethods[path].includes(method)) {
+
+        // Get the allowed methods for the exact path or the modified path
+        const methods = allowedMethods[path] || allowedMethods[pathWithParams];
+        // Check if the method is allowed for the path
+        if (!methods.includes(method)) {
             return next(new MethodNotAllowedError(`${method} method not allowed in ${path}`));
         }
+
         next();
     };
 };
