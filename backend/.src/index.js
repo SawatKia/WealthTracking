69f862c5ab9fd7c4fef24424c80a98e45af4430e
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2024-08-07 18:19:47 +0700

    set ratelimit for duction env  and remove cors

--- backend/.src/index.js
+++ backend/.src/index.js
@@ -14,11 +14,11 @@ const formatResponse = require('./utils/responseFormatter');
 const logger = new Logging('index');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const isDevelopment = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === 'development';
 const router = express.Router();
 const limiter = rateLimit({
-    windowMs: 1000, // 1 second
-    limit: 2, // Limit each IP to 2 requests per `window` (here, per 15 minutes)
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // limit each IP to 100 requests per windowMs
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     message: 'Too many requests from this IP, please try again later.',
@@ -27,24 +27,23 @@ const limiter = rateLimit({
         res.status(options.statusCode).json(formatResponse(options.statusCode, options.message));
     },
 })
-
-app.use(limiter);
-app.use(cors());
+!isDev && app.use(limiter);
+//app.use(cors());
 app.use(express.json());
 app.disable('x-powered-by');
 // Log middleware
 app.use((req, res, next) => {
     const { ip, method, path } = req;
-    const allowedMethods = ["GET", "POST", "PUT", "PATCH", "DELETE"];
+    const debugMethods = ["GET", "POST", "PUT", "PATCH", "DELETE"];
 
-    if (allowedMethods.includes(req.method)) {
+    if (debugMethods.includes(req.method)) {
         logger.debug(`Incoming Request, ${ip} => ${method} ${path}   with ${JSON.stringify(req.body)}`);
     } else {
         logger.silly(`Incoming Request: Method=${method}, Path=${path}`);
     }
 
     res.on('finish', () => {
-        if (allowedMethods.includes(req.method)) {
+        if (debugMethods.includes(req.method)) {
             logger.debug(`Outgoing response, ${path} => ${res.statusCode} ${res.statusMessage} => ${ip} `);
         } else {
             logger.silly(`Outgoing response: Method=${method}, Path=${path}, Status=${res.statusCode}`);
@@ -71,7 +70,7 @@ const startServer = async () => {
         await connectDB();
         app.listen(PORT, '0.0.0.0', () => {
             logger.debug(`App listening on port ${PORT}`);
-            if (isDevelopment) {
+            if (isDev) {
                 logger.info('Starting server in debug mode...');
             } else {
                 logger.info('Starting Production server...');
