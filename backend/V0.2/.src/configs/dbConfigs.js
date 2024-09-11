75e4cd5066bacfe3581962c09927e6e4864c6467
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2024-09-11 09:13:38 +0700

    ensure is string

--- backend/V0.2/.src/configs/dbConfigs.js
+++ backend/V0.2/.src/configs/dbConfigs.js
@@ -7,7 +7,7 @@ module.exports = {
         user: process.env.POSTGRES_USER,
         host: process.env.POSTGRES_HOST,
         database: process.env.POSTGRES_NAME,
-        password: process.env.POSTGRES_PASSWORD,
+        password: String(process.env.POSTGRES_PASSWORD),
         port: process.env.POSTGRES_PORT,
         max: 20, // maximum number of connections in the pool connection
         idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
