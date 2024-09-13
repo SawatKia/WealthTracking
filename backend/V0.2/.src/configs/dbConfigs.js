a78e73aef2bc787104dafd4acc85e0e1bbb333c7
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2024-09-13 11:57:24 +0700

    update config

--- backend/V0.2/.src/configs/dbConfigs.js
+++ backend/V0.2/.src/configs/dbConfigs.js
@@ -14,15 +14,23 @@ module.exports = {
         connectionTimeoutMillis: 2000, // how long to wait for a connection to be established
     },
     test: {
+        user: process.env.POSTGRES_USER,
+        host: process.env.POSTGRES_HOST,
         database: process.env.POSTGRES_TEST_NAME,
-        user: process.env.POSTGRES_TEST_USER,
-        password: process.env.POSTGRES_TEST_PASSWORD,
-        host: process.env.POSTGRES_TEST_HOST,
+        password: String(process.env.POSTGRES_PASSWORD),
+        port: process.env.POSTGRES_PORT,
+        max: 20, // maximum number of connections in the pool connection
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+        connectionTimeoutMillis: 2000, // how long to wait for a connection to be established
     },
     production: {
-        database: process.env.POSTGRES_PROD_NAME,
         user: process.env.POSTGRES_PROD_USER,
-        password: process.env.POSTGRES_PROD_PASSWORD,
-        host: process.env.POSTGRES_PROD_HOST,
+        host: process.env.POSTGRES_HOST,
+        database: process.env.POSTGRES_PROD_NAME,
+        password: String(process.env.POSTGRES_PROD_PASSWORD),
+        port: process.env.POSTGRES_PORT,
+        max: 20, // maximum number of connections in the pool connection
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+        connectionTimeoutMillis: 2000, // how long to wait for a connection to be established
     }
 }
