54b11ce5b4ad37d66e8d4a5e020cbfb773ac8bbc
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2024-11-02 15:15:32 +0700

    add cascade syntax for SQL creating tables

--- backend/V0.2/.src/services/PgClient.js
+++ backend/V0.2/.src/services/PgClient.js
@@ -185,7 +185,6 @@ class PgClient {
 
   async createTableIfNotExist(tableName) {
     const createTableQuery = {
-      //TODO -  add DoB to users table
       users: `CREATE TABLE IF NOT EXISTS users (
               national_id CHAR(13) PRIMARY KEY,
               email VARCHAR(255) NOT NULL,
@@ -193,7 +192,7 @@ class PgClient {
               hashed_password VARCHAR(255) NOT NULL,
               role VARCHAR(20) NOT NULL,
               member_since TIMESTAMP NOT NULL,
-              date_of_birth DATE,  -- New DoB field
+              date_of_birth DATE,
               CONSTRAINT check_national_id_length CHECK (LENGTH(national_id) = 13)
           );`,
       financial_institutions: `CREATE TABLE IF NOT EXISTS financial_institutions (
@@ -209,8 +208,10 @@ class PgClient {
                 account_name VARCHAR(100) NOT NULL,
                 balance DECIMAL(15, 2) NOT NULL,
                 PRIMARY KEY (account_number, fi_code),
-                FOREIGN KEY (national_id) REFERENCES users(national_id),
-                FOREIGN KEY (fi_code) REFERENCES financial_institutions(fi_code) ON UPDATE CASCADE ON DELETE CASCADE
+                FOREIGN KEY (national_id) REFERENCES users(national_id) 
+                    ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (fi_code) REFERENCES financial_institutions(fi_code) 
+                    ON UPDATE CASCADE ON DELETE CASCADE
             );`,
       debts: `CREATE TABLE IF NOT EXISTS debts (
                 debt_number VARCHAR(50) NOT NULL,
@@ -223,8 +224,10 @@ class PgClient {
                 loan_principle DECIMAL(15, 2) NOT NULL,
                 loan_balance DECIMAL(15, 2) NOT NULL,
                 PRIMARY KEY (debt_number, fi_code),
-                FOREIGN KEY (national_id) REFERENCES users(national_id),
-                FOREIGN KEY (fi_code) REFERENCES financial_institutions(fi_code) ON UPDATE CASCADE ON DELETE CASCADE
+                FOREIGN KEY (national_id) REFERENCES users(national_id)
+                    ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (fi_code) REFERENCES financial_institutions(fi_code) 
+                    ON UPDATE CASCADE ON DELETE CASCADE
             );`,
       transactions: `CREATE TABLE IF NOT EXISTS transactions (
                 transaction_id SERIAL PRIMARY KEY,
@@ -236,8 +239,10 @@ class PgClient {
                 national_id CHAR(13) NOT NULL,
                 debt_number VARCHAR(50),
                 fi_code VARCHAR(20),
-                FOREIGN KEY (national_id) REFERENCES users(national_id),
-                FOREIGN KEY (debt_number, fi_code) REFERENCES debts(debt_number, fi_code) ON UPDATE CASCADE ON DELETE CASCADE
+                FOREIGN KEY (national_id) REFERENCES users(national_id)
+                    ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (debt_number, fi_code) REFERENCES debts(debt_number, fi_code) 
+                    ON UPDATE CASCADE ON DELETE CASCADE
             );`,
       transaction_bank_account_relations: `CREATE TABLE IF NOT EXISTS transaction_bank_account_relations (
                 transaction_id INT NOT NULL,
@@ -245,8 +250,10 @@ class PgClient {
                 fi_code VARCHAR(20) NOT NULL,
                 role VARCHAR(20) NOT NULL,
                 PRIMARY KEY (account_number, fi_code, transaction_id),
-                FOREIGN KEY (account_number, fi_code) REFERENCES bank_accounts(account_number, fi_code) ON UPDATE CASCADE ON DELETE CASCADE,
-                FOREIGN KEY (transaction_id) REFERENCES transactions(transaction_id) ON UPDATE CASCADE ON DELETE SET NULL
+                FOREIGN KEY (account_number, fi_code) REFERENCES bank_accounts(account_number, fi_code) 
+                    ON UPDATE CASCADE ON DELETE CASCADE,
+                FOREIGN KEY (transaction_id) REFERENCES transactions(transaction_id) 
+                    ON UPDATE CASCADE ON DELETE CASCADE
             );`,
       api_request_limits: `CREATE TABLE IF NOT EXISTS api_request_limits (
                 service_name VARCHAR(255) NOT NULL,
