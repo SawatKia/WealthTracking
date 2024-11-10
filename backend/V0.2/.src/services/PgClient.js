58b4391e3c217067d1fb1daf110e1401b7d6a274
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2024-11-10 18:53:20 +0700

    modify create table query

--- backend/V0.2/.src/services/PgClient.js
+++ backend/V0.2/.src/services/PgClient.js
@@ -187,14 +187,20 @@ class PgClient {
   async createTableIfNotExist(tableName) {
     const createTableQuery = {
       users: `CREATE TABLE IF NOT EXISTS users (
-              national_id CHAR(13) PRIMARY KEY,
+              national_id VARCHAR(255) PRIMARY KEY,
               email VARCHAR(255) NOT NULL,
               username VARCHAR(50) NOT NULL,
-              hashed_password VARCHAR(255) NOT NULL,
+              hashed_password VARCHAR(255),
               role VARCHAR(20) NOT NULL,
               member_since TIMESTAMP NOT NULL,
               date_of_birth DATE,
-              CONSTRAINT check_national_id_length CHECK (LENGTH(national_id) = 13)
+              auth_service VARCHAR(20) DEFAULT 'local',
+              profile_picture_uri VARCHAR(255),
+              CONSTRAINT check_auth_service CHECK (auth_service IN ('local', 'google', 'facebook', 'apple')),
+              CONSTRAINT check_password_requirement CHECK (
+                  (auth_service = 'local' AND hashed_password IS NOT NULL) OR 
+                  (auth_service != 'local')
+              )
           );`,
       financial_institutions: `CREATE TABLE IF NOT EXISTS financial_institutions (
                 fi_code VARCHAR(20) PRIMARY KEY,
