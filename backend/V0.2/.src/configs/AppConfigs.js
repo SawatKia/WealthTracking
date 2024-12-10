b4356f34af1de9dcc7ea60df755ff27c9c36ca15
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2024-12-10 21:24:50 +0700

    add new env

--- backend/V0.2/.src/configs/AppConfigs.js
+++ backend/V0.2/.src/configs/AppConfigs.js
@@ -45,6 +45,7 @@ const config = {
         host: process.env.OLLAMA_HOST || 'http://localhost:11434',
         model: process.env.OLLAMA_MODEL || 'llama3.2',
     },
+    loadMockData: process.env.LOAD_MOCK_DATA || false,
 };
 
 module.exports = config;
