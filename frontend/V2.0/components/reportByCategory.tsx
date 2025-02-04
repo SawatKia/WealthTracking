8d7fddce4ef632d4dcbc24d782b1895d9dba9d23
DaoRungphailin <88391247+DaoRungphailin@users.noreply.github.com>
2025-02-04 15:12:27 +0700

    decorate pie chart

--- frontend/V2.0/components/reportByCategory.tsx
+++ frontend/V2.0/components/reportByCategory.tsx
@@ -1,39 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
-import { VictoryPie, VictoryTheme } from 'victory-native';
-import { useTransactions } from '../services/TransactionService'; 
+import React, { useEffect, useState } from "react";
+import { View, Text, ActivityIndicator } from "react-native";
+import { VictoryPie, VictoryTheme, VictoryLabel } from "victory-native";
+import { useTransactions } from "../services/TransactionService";
 
-  const ReportByCategory = () => {
-    const { monthlyExpenses, loading, error } = useTransactions(); 
-    const [chartData, setChartData] = useState<any[]>([]);
+const ReportByCategory = () => {
+  const { monthlyExpenses, loading, error } = useTransactions();
+  const [chartData, setChartData] = useState<any[]>([]);
 
-    useEffect(() => {
-      if (monthlyExpenses) {
-        const data = monthlyExpenses.map((expense: any) => ({
-          x: expense.type,  
-          y: expense.totalAmount, 
-        }));
-        setChartData(data); 
-      }
-    }, [monthlyExpenses]);
-    
-    // Log after state update to ensure it's up-to-date
-    useEffect(() => {
-      console.log('Updated chart data:', chartData); 
-    }, [chartData]);
-    
+  useEffect(() => {
+    if (monthlyExpenses) {
+      const data = monthlyExpenses.map((expense: any) => ({
+        x: expense.type,
+        y: expense.totalAmount,
+      }));
+      setChartData(data);
+    }
+  }, [monthlyExpenses]);
+
+  // Log after state update to ensure it's up-to-date
+  useEffect(() => {
+    console.log("Updated chart data:", chartData);
+  }, [chartData]);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 20, marginBottom: 20, marginTop: 30, alignSelf: 'flex-end' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      {/* <Text
+        style={{
+          fontSize: 20,
+          marginBottom: 20,
+          marginTop: 30,
+          alignSelf: "flex-end",
+        }}
+      >
         Link to budget page
-      </Text>
+      </Text> */}
+      <Text style={{ fontSize: 20,fontWeight: 'bold', marginTop: 35 }}>Expenses By Category</Text>
+      
       <VictoryPie
-        data={chartData}        
+        data={chartData}
         theme={VictoryTheme.clean}
-        colorScale={["#4957AA", "#7F8CD9", "#9AC9F3"]} 
-        // colorScale={["#FF8C00", "#FF6347", "#87CEEB"]} 
-        labelRadius={145} 
+        // innerRadius={68}
+        labelRadius={150}
+        colorScale={["#4957AA", "#7F8CD9", "#9AC9F3"]}
+        // colorScale={["#FF8C00", "#FF6347", "#87CEEB"]}
         style={{
           labels: {
             fontSize: 10,
@@ -41,14 +50,23 @@ import { useTransactions } from '../services/TransactionService';
           },
           data: {
             fillOpacity: 0.9,
-            backgroundColor: 'transparent', 
-          },
-          parent: {
-            backgroundColor: '#fff',  
-            opacity: 0.5,
+            borderWidth: 20,
             borderRadius: 20,
           },
+          parent: {
+            backgroundColor: "transparent",
+            borderRadius: 20,
+            padding: 2,
+          },
         }}
+      />
+      <VictoryLabel
+        textAnchor="middle"
+        style={{ fontSize: 14 }}
+        x={200}
+        y={200}
+        text="Expense"
+
       />
     </View>
   );
