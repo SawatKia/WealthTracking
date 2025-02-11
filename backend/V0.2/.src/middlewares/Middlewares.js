7a1c42534ef40580f9d0edc3f21775c8843bdb46
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2025-02-11 18:54:40 +0700

    use helper function to get client ip

--- backend/V0.2/.src/middlewares/Middlewares.js
+++ backend/V0.2/.src/middlewares/Middlewares.js
@@ -224,17 +224,36 @@ class Middlewares {
     });
   }
 
-  /**
- * Request logger middleware to log incoming requests in a consistent format
- */
   requestLogger(req, res, next) {
     logger.info(`entering the routing for ${req.method} ${req.url}`);
+
+    const getIP = (req) => {
+      // req.connection is deprecated
+      const conRemoteAddress = req.connection?.remoteAddress;
+      // req.socket is said to replace req.connection
+      const sockRemoteAddress = req.socket?.remoteAddress;
+      // some platforms use x-real-ip
+      const xRealIP = req.headers['x-real-ip'];
+      // most proxies use x-forwarded-for
+      const xForwardedForIP = (() => {
+        const xForwardedFor = req.headers['x-forwarded-for'];
+        if (xForwardedFor) {
+          // The x-forwarded-for header can contain a comma-separated list of
+          // IP's. Further, some are comma separated with spaces, so whitespace is trimmed.
+          const ips = xForwardedFor.split(',').map((ip) => ip.trim());
+          return ips[0];
+        }
+      })();
+      // prefer x-forwarded-for and fallback to the others
+      return xForwardedForIP || xRealIP || sockRemoteAddress || conRemoteAddress;
+    };
+
     const method = req.method;
     const requestPath = req.path;
     const body = req.body;
     const headers = req.headers;
     const query = req.query;
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || '0.0.0.0';
+    const ip = getIP(req);
 
     // Prepare the body for logging 
     let logBody;
