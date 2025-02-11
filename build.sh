91ae8de17bd819bfff6b5f1b8b58c46d89119f0d
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2025-02-11 19:22:17 +0700

    apply -e to echo todusplay colors

--- build.sh
+++ build.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 if [ "${PWD##*/}" != "WealthTracking" ]; then
-    echo "\033[1;31m>>> Please run this script from the WealthTracking directory.\033[0m"
+    echo -e "\033[1;31m>>> Please run this script from the WealthTracking directory.\033[0m"
     exit 1
 fi
 
 if ! command -v npm &> /dev/null; then
-    echo "\033[1;34m>>>\033[0m npm is not installed."
+    echo -e "\033[1;34m>>>\033[0m npm is not installed."
     exit 1
 fi
 
 cd backend/V0.2 || exit
-echo "\033[1;34m>>>\033[0m Installing dependencies..."
+echo -e "\033[1;34m>>>\033[0m Installing dependencies..."
 npm ci
 
-echo "\033[1;34m>>>\033[0m Building the application using Babel..."
+echo -e "\033[1;34m>>>\033[0m Building the application using Babel..."
 npm run build
 
 # Change back to the WealthTracking root directory
 cd ../../
 
-echo "\033[1;34m>>>\033[0m Building Docker images..."
+echo -e "\033[1;34m>>>\033[0m Building Docker images..."
 set -a
 source ./.env
 set +a
