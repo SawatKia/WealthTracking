e4d5751b00f9973d423e3d99a81d7cb5ecf57417
DaoRungphailin <88391247+DaoRungphailin@users.noreply.github.com>
2025-02-17 14:54:20 +0700

    edit base url

--- frontend/V2.0/services/axiosInstance.tsx
+++ frontend/V2.0/services/axiosInstance.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { storeToken, getToken, deleteToken } from "./AuthenService"; // Utility functions for token management
 // http://161.246.5.86
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v0.2", // Replace with your backend URL
+  baseURL: "http://161.246.5.86/api/v0.2", // Replace with your backend URL
   headers: {
     "Content-Type": "application/json",
   },
