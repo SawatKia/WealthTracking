4a6084ed9967c9b57f1d03fc9142d3e7f37335d5
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2025-02-27 13:02:40 +0700

    remove build command

--- build.sh
+++ build.sh
@@ -5,23 +5,8 @@ if [ "${PWD##*/}" != "WealthTracking" ]; then
     exit 1
 fi
 
-if ! command -v npm &> /dev/null; then
-    echo -e "\033[1;31mnpm is not installed.\033[0m"
-    exit 1
-fi
-
-cd backend/V0.2 || exit
-echo -e "\033[7;34m>>>\033[0m Installing dependencies..."
-npm ci
-
-echo -e "\033[7;34m>>>\033[0m Building the application using Babel..."
-npm run build
-
-# Change back to the WealthTracking root directory
-cd ../../
-
 echo -e "\033[7;34m>>>\033[0m Building Docker images..."
 set -a
 source ./.env
 set +a
-docker compose -f docker-compose.prod.yml build --no-cache
+docker compose -f docker-compose.yml build --no-cache
