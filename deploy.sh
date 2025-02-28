bb388b9fddeffe1aa7607fc6f301846ca5d095fd
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2025-02-28 19:16:44 +0700

    add waitimg period after git pull

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+sleepWithTimer() {
+    for i in $(seq $1 -1 1); do
+        printf "\r\033[34mCountdown: %2d\033[0m   " "$i"
+        sleep 1
+    done
+    echo -e "\n"
+}
+
 echo -e "\033[7;34m>>>\033[0m verifying the current directory..."
 if [ "${PWD##*/}" != "WealthTracking" ]; then
   echo -e "\033[1;31mPlease run this script from the WealthTracking directory.\033[0m"
@@ -14,6 +22,7 @@ if [ $? -ne 0 ]; then
   echo -e "\033[1;31mFailed to pull git repo. Exiting...\033[0m"
   exit 1
 fi
+sleepWithTimer 3
 
 if [ ! -f "./installing.sh" ] || [ ! -f "./start_server.sh" ]; then
   echo -e "\033[5;31mFiles installing.sh and start_server.sh do not exist. Exiting...\033[0m"
