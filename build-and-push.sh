5e915a508d3cde438c89a268097481b3a27a6823
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2025-03-27 11:56:29 +0700

    add latest

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,3 +1,9 @@
+#  The script is simple and straightforward. It takes a single argument, which is the tag name. The script then builds the Docker image, tags it with the tag name and latest, and pushes it to Docker Hub. 
+
+#  To run the script, execute the following command: 
+#  $ ./build-and-push.sh 1.0.2
+#  The script will build the Docker image, tag it with 1.0.2 and latest, and push it to Docker Hub. 
+ 
 #!/bin/bash
 
 if [ -z "$1" ]; then
@@ -15,10 +21,11 @@ cd backend/V0.2/
 docker build -t swtl918/wealthtracking:$TAG_NAME .
 
 # tagging
-docker tag swtl918/wealthtracking:$TAG_NAME swtl918/wealthtracking:lastest
+docker tag swtl918/wealthtracking:$TAG_NAME swtl918/wealthtracking:latest
 
 # Push to Docker Hub
 docker push swtl918/wealthtracking:$TAG_NAME 
-docker push swtl918/wealthtracking:lastest
+docker push swtl918/wealthtracking:latest
 
-echo "Successfully built and pushed swtl918/wealthtracking:$TAG_NAME"
+echo "Successfully built and pushed \033[1;32mswtl918/wealthtracking:$TAG_NAME\033[0m and \033[1;32mswtl918/wealthtracking:latest\033[0m to Docker Hub"
+ 
